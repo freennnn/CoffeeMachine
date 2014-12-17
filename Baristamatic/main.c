64a28274f05d6a4bc74c4550e032251f087176ea
freennnnn <freennnn@gmail.com>
2014-12-16 18:46:33 -0600

    Initial Commit

---
+++ Baristamatic/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Baristamatic
+//
+//  Created by alexander.pranevich on 12/16/14.
+//  Copyright (c) 2014 AP. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
